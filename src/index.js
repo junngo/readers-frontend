8bde991c142f49b1b80c81f7c576cd43dc4c9eb4
junngo <myeongjun.ko@gmail.com>
2021-09-03 01:10:12 +0900

    created user reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// console.log(localStorage.getItem("isLoggedIn"));
+console.log(store.getState());
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
