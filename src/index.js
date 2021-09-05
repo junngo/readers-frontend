283affdfba39ab4533c6743c011d4b1ecfaa46b3
junngo <myeongjun.ko@gmail.com>
2021-09-05 15:16:40 +0900

    added history and connected it with router for unsing hsitory in reducer. History can be syncing with router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import store from "redux/configureStore";
+import { ConnectedRouter } from "connected-react-router";
+import store, { history } from "redux/configureStore";
 import 'index.css';
 import App from 'App';
 import reportWebVitals from 'reportWebVitals';
@@ -15,7 +16,9 @@ store.dispatch({ type: "HELLO" })
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
 import users from "redux/modules/user";
 
 const env = process.env.NODE_ENV;
 
-const middlewares = [thunk];
+const history = createBrowserHistory();
+
+const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === "development") {
     const { logger } = require("redux-logger");
@@ -12,10 +16,13 @@ if (env === "development") {
 }
 
 const reducer = combineReducers({
-    users
+    users,
+    router: connectRouter(history)
 })
 
 let store = initialState => 
     createStore(reducer, applyMiddleware(...middlewares));
+
+export  { history };
 
 export default store();
